18ae83455143bce062eef9957999c582dae37d0a
Tormi_Arne <101526512+Tormi-A-Raidvere@users.noreply.github.com>
2026-04-01 20:56:26 +0300

    Kaardi klass

---
+++ Kaart.java
@@ -0,0 +1,9 @@
+public class Kaart {
+    private String esikylg;
+    private String tagakylg;
+
+    public Kaart(String esikylg, String tagakylg) {
+        this.esikylg = esikylg;
+        this.tagakylg = tagakylg;
+    }
+}
